25ff335719cf79d9cd78a8a34918081d237b08fc
heidelpay-sp <sascha.pflueger@heidelpay.de>
2017-12-18 13:05:42 +0100

    - changed versionnumber and update()

--- Frontend/HeidelGateway/Bootstrap.php
+++ Frontend/HeidelGateway/Bootstrap.php
@@ -25,7 +25,7 @@ class Shopware_Plugins_Frontend_HeidelGateway_Bootstrap extends Shopware_Compone
 	 * @return string version number
 	 */
 	public function getVersion(){
-		return '17.12.15';
+		return '17.12.18';
 	}
 
 	/**
@@ -750,6 +750,15 @@ class Shopware_Plugins_Frontend_HeidelGateway_Bootstrap extends Shopware_Compone
                         )
                     );
 
+                    $msg .= '* update 17.12.15 <br />';
+                } catch (Exception $e) {
+                    $this->logError($msg, $e);
+                }
+
+            case '17.12.18':
+                // Hotfix for switching to other PaymentMethods
+                // Fix in BackendHgw for fin-transactions by payolution and santander and IVPG
+                try{
                     $msg .= '* update 17.12.15 <br />';
                 } catch (Exception $e) {
                     $this->logError($msg, $e);
